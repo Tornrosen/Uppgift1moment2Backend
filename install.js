42475dc968992ba44a95ad2c791c3d382cc27456
Tornrosen <elec11anna@hotmail.com>
2025-04-18 12:08:04 +0200

    skapa databas

---
+++ install.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection( {
+    host: "localhost",
+    user: "jobs",
+    password: "workExperience",
+    database: "jobs"
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Connection failed: " + err);
+        return;
+    }
+    console.log("Connected to MySQL!")
+})
+
+connection.query("DROP TABLE IF EXISTS jobs;", (err, results) => {
+    if (err) throw err;
+    console.log("Table deleted!")
+})
+
+connection.query(`CREATE TABLE jobs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    workplace VARCHAR(100),
+    employer VARCHAR(10),
+    startdate VARCHAR(100),
+    enddate VARCHAR(100),
+    description VARCHAR(250),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP)`, (err, results) => {
+    if (err) throw err;
+    console.log("Table created!")
+}
+)
+
